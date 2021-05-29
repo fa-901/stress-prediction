a39bfc6df0aefe85c9c09f7925d316aadf7280fd
Farhan Alam <metalhead901@live.com>
2021-05-29 16:19:45 +0600

    add text field and button

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
 
-import React, { Fragment } from "react";
-import { Typography, Button, AppBar, Toolbar, CssBaseline, makeStyles } from '@material-ui/core';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
+import React, { useState } from "react";
+import { Typography, Button, AppBar, Toolbar, CssBaseline, makeStyles, Container, Box, TextField } from '@material-ui/core';
+import { CameraIcon, Assessment } from '@material-ui/icons';
 
-// const useStyles = makeStyles((theme) => ({
-// 	icon: {
-// 		marginRight: theme.spacing(2),
-// 	},
-// }));
-class App extends React.Component {
-	constructor(props) {
-		super(props);
+const useStyles = makeStyles((theme) => ({
+	button: {
+		"marginBottom": theme.spacing(5),
+		"color": "#fff",
+		"backgroundColor": "#21D4FD",
+		"backgroundImage": "linear-gradient(270deg, #21D4FD 0%, #B721FF 86%)"
+	},
+	icon: {
+		marginRight: theme.spacing(2),
+	},
+	container: {
+		height: '100%',
+		marginTop: theme.spacing(10)
+	},
+	text: {
+		width: '100%',
+		marginBottom: theme.spacing(2),
+		resize: 'vertical'
+	},
+}));
 
-		this.state = {
-			pss: 0,
-			txt: '',
-		}
-	}
+function App() {
+	const classes = useStyles();
 
-	render() {
-
-		return (
-			<CssBaseline>
-				<AppBar position="relative">
-					<Toolbar>
-						<CameraIcon />
-						<Typography variant="h6" color="inherit" noWrap>
-							Title
-						</Typography>
-					</Toolbar>
-				</AppBar>
-				<h1>
-					Hello
-				</h1>
-				<Button variant="contained">this is a material UI button</Button>
-			</CssBaseline>
-		);
-	}
+	return (
+		<CssBaseline>
+			<AppBar position="relative">
+				<Toolbar>
+					<Assessment className={classes.icon} />
+					<Typography variant="h6" color="inherit" noWrap>
+						Stress Predictor
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<main>
+				<Container maxWidth="sm" className={classes.container}>
+					<Box display="flex" flexDirection='column' justifyContent="center" alignItems="center">
+						<TextField
+							className={classes.text}
+							label="Add Text Here"
+							multiline
+							rows={6}
+							variant="outlined"
+						/>
+						<Button variant="contained" className={classes.button}>
+							Get Your Score
+						</Button>
+					</Box>
+				</Container>
+			</main>
+		</CssBaseline>
+	);
 }
 
 export default App;
