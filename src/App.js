0b375c0bda5aeaf9bc7775a8bd445936f3756313
Farhan Alam <metalhead901@live.com>
2021-05-29 14:35:20 +0600

    work on header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 
-import React from "react";
-import Button from '@material-ui/core/Button';
+import React, { Fragment } from "react";
+import { Typography, Button, AppBar, Toolbar, CssBaseline, makeStyles } from '@material-ui/core';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
 
+// const useStyles = makeStyles((theme) => ({
+// 	icon: {
+// 		marginRight: theme.spacing(2),
+// 	},
+// }));
 class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-        <Button variant="contained">this is a material UI button</Button>
-      </>
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			pss: 0,
+			txt: '',
+		}
+	}
+
+	render() {
+
+		return (
+			<CssBaseline>
+				<AppBar position="relative">
+					<Toolbar>
+						<CameraIcon />
+						<Typography variant="h6" color="inherit" noWrap>
+							Title
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<h1>
+					Hello
+				</h1>
+				<Button variant="contained">this is a material UI button</Button>
+			</CssBaseline>
+		);
+	}
 }
 
 export default App;
